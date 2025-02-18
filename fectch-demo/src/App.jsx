ec52bb18f200cb8c623f6362a28fdc1c68180771
Richard Y. Hayashi <richardyhayashi@gmail.com>
2025-02-18 17:38:33 +0900

    update

--- fectch-demo/src/App.jsx
+++ fectch-demo/src/App.jsx
@@ -1,18 +1,11 @@
 import ContextDemo from "./components/ContextDemo";
 //import FetchDemo from "./components/FetchDemo";
-//import ReactQueryDemo from "./components/ReactQueryDemo";
-//import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-//const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
       <ContextDemo />
       {/*<FetchDemo />*/}
-      {/*<QueryClientProvider>
-        <ReactQueryDemo />
-      </QueryClientProvider>*/}
     </>
   );
 }
--- fectch-demo/src/components/ReactQueryDemo.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ReactQueryDemo = () => {
-  return (
-    <div>ReactQueryDemo</div>
-  );
-};
-
-export default ReactQueryDemo;
